c09ed2a141de783e9ab761eaa18851d50b99eed8
Victor Tsui <33289453+LastsForever@users.noreply.github.com>
2022-05-01 18:40:30 +0800

    Update OfficeItemHandlerForRamda.js

--- OfficeItemHandlerForRamda.js
+++ OfficeItemHandlerForRamda.js
@@ -1,4 +1,6 @@
 //  WPS Office Collection Handler for Ramda (v0.0.1)
+//	(github：https://github.com/LastsForever/wps_office_collection_handler_for_ramda)	
+//	
 //  通过对 WPS Office集合对象（需带有Item方法）使用代理，使之可被Ramda库的R.map与R.filter直接操作。
 //  使用方法：
 //  	(1) 新建一个WPS JS宏代码模块，复制Ramda源码(dist/ramda.js文件)至其中;
